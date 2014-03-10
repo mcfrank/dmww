43194b385419a94f78132e82a268f72a2beb205a
Molly Lewis <mll@stanford.edu>
2014-03-09 18:40:23 -0700

    working visualization of lexicons, need to work on function implementation

--- plotting_helper.py
+++ plotting_helper.py
@@ -1,25 +1,26 @@
 # import stuff
 import matplotlib.pyplot as plt
-import prettyplotlib as ppl
 import matplotlib.gridspec as gridspec
 
 def lexplot(l, w):
-    fig, ax = ppl.subplots(1)
-    gs = gridspec.GridSpec(2, 1,height_ratios=[8,1])
-    ax1 = plt.subplot(gs[0])
-    ax2 = plt.subplot(gs[1])
 
-    w_labels = "".join(map(str, range(0, w.n_words)))
-    o_labels = "".join(map(str, range(0, w.n_objs)))
-
-    #referential lexicon
-    ppl.pcolormesh(fig, ax1, l.ref, xticklabels = w_labels, yticklabels = o_labels) 
+    fig = plt.figure()
+    gs = gridspec.GridSpec(2, 1, height_ratios=[8,1])
+    
+    ax1 = fig.add_subplot(gs[0])
+    ax1.pcolormesh(l.ref, cmap = "Reds") 
+    ax1.set_xticks(np.arange(w.n_words) + .5)
+    ax1.set_xticklabels(np.arange(0,w.n_words))
+    ax1.set_yticks(np.arange(w.n_objs) + .5)
+    ax1.set_yticklabels(np.arange(0,w.n_objs))
     ax1.set_ylabel("objects")
     ax1.set_title('main lexicon', fontsize=14)
 
-    #non-referential lexicon
-    ppl.pcolormesh(fig, ax2, array([l.non_ref]), xticklabels = w_labels)
+    ax2 = fig.add_subplot(gs[1])
+    ax2.pcolormesh(np.array([l.non_ref]), cmap = "Reds") 
+    ax2.set_xticks(np.arange(w.n_words) + .5)
+    ax2.set_xticklabels(np.arange(0,w.n_words))
+    ax2.set_yticks([])
     ax2.set_xlabel("words")
     ax2.set_title('non-referential lexicon', fontsize=14)
-    ax2.set_yticks([])
     plt.tight_layout(pad = 2)
