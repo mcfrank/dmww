bbc1c597d85dbb213784ec3424340322e680c919
Mika Braginsky <mikabr@stanford.edu>
2014-11-08 16:49:00 -0800

    added multiple chains to grid sim

--- grid_sim.sh
+++ grid_sim.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # run a grid of dmww simulations using given algorithm and number of samples/particles
-# run with ./grid_sim.sh <algorithm> <number of samples/particles>
+# run with ./grid_sim.sh <algorithm> <number of samples/particles> <number of chains>
 
-ar_opts="1.0 0.1 0.01 0.001 0.0001"
-an_opts="100 10 1.0 0.1 0.01"
+ar_opts="10 1.0 0.1 0.01"
+an_opts="100 10 1.0 0.1"
 ei_opts="0.1 0.01 0.001"
 
-for ar in $ar_opts
+for c in $(seq 1 $3)
 do
-  for an in $an_opts
+  for ar in $ar_opts
   do
-    for ei in $ei_opts
+    for an in $an_opts
     do
-      qsub simulation.sh $1 $2 $ar $an $ei
+      for ei in $ei_opts
+      do
+        qsub simulation.sh $1 $2 $ar $an $ei
+      done
     done
   done
 done
